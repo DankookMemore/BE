e2a4d44850b441c68f42eafa24a4317b75646b84
blueoxygens <exboxer234@gmail.com>
2025-05-13 16:12:24 +0900

    [chore] 파일 구조 재구성, 프론트와 백엔드 분리

--- src/utils/ReminderHandler.ts
+++
@@ -1,9 +0,0 @@
-import * as Notifications from 'expo-notifications';
-
-export const setupNotificationResponseListener = (navigationRef: any) => {
-  return Notifications.addNotificationResponseReceivedListener(() => {
-    if (navigationRef.isReady()) {
-      navigationRef.navigate('MemoList');
-    }
-  });
-};
